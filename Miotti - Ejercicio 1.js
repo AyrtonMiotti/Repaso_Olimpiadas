4109fdae19faca54137d5a0a43773d246e0a9dbd
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-29 12:49:53 -0300

    Update Miotti - Ejercicio 1.js

--- Miotti - Ejercicio 1.js
+++ Miotti - Ejercicio 1.js
@@ -1,19 +1,4 @@
-/**
- * Armar un programa que imprima una piramide de Mario Bros. El programa debe:
- *  -Tomar como entrada la altura de la piramide(INT entre 2 y 8 inclusive).
- *  -Imprimir dos semi piramides de la altura especificada con '#' que esten separadas por dos espacios.
- *   EJEMPLO:
- *       # #
-        ## ##
-       ### ###
-      #### ####
-     ##### #####
-    ###### ######
-   ####### #######
-  ######## ########
- ######### #########
-########## ##########
-*/
+// EJERCICIO 1
 function Pyramid(height){
     let spaces = height;
     for(let i=0; i<=height; i++){
