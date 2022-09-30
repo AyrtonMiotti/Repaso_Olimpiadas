2ae2763b60c5e93d104a5f80bbbba8c430c0f2ff
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-30 08:33:57 -0300

    Repaso_Olimpiadas
    Add excercise 3.

---
+++ Miotti - Ejercicio 3.js
@@ -0,0 +1,4 @@
+var arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+arr1.sort(function() { return Math.random() - 0.5 });
+console.log(arr1);
+
