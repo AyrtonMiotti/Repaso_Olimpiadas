102d3f7dd15b9fa2390c4d06f528fa85863e3f37
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-30 20:48:42 -0300

    Update Miotti - Ejercicio 3.js

--- Miotti - Ejercicio 3.js
+++ Miotti - Ejercicio 3.js
@@ -1,10 +1,19 @@
-var arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-arr2 = arr1.sort(function() { return Math.random() - 0.5 });
-console.log(arr2)
-
-
 function encript(message){
-  for(let x = 0; x < arr1.lenght; x++){
-    console.log(message.split(''));
-  } 
+  let mensaje = []
+  let newMessage = ""
+  let arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].sort(function() { return Math.random() - 0.5 });
+  let arr2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  mensaje = message.split('');
+  for(let x = 0; x < mensaje.length; x++){
+    for(let i = 0; i < arr1.length; i++){
+      if(mensaje[x] == arr1[i]){
+        newMessage = newMessage + arr2[i];
+      }
+    }
+  }
+  console.log('\n Texto plano: ' + message + '\n Texto cifrado: ' + newMessage);
+} 
+
+for(let i = 0; i<10; i++){
+  encript("abcdefghijklmnñopqrstuvwxyz")
 }
