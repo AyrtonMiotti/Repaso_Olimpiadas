6b3512a94816847372f97306bebf7285a60ba6e5
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-30 09:26:16 -0300

    Update Miotti - Ejercicio 3.js

--- Miotti - Ejercicio 3.js
+++ Miotti - Ejercicio 3.js
@@ -1,4 +1,10 @@
 var arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-arr1.sort(function() { return Math.random() - 0.5 });
-console.log(arr1);
+arr2 = arr1.sort(function() { return Math.random() - 0.5 });
+console.log(arr2)
 
+
+function encript(message){
+  for(let x = 0; x < arr1.lenght; x++){
+    console.log(message.split(''));
+  } 
+}
